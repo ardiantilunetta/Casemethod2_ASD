36183b57fbb77c4cf17e164dad9cf7ba3f705ee9
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2026-05-15 23:30:29 +0700

    class pesanan

---
+++ Pesanan18.java
@@ -0,0 +1,11 @@
+public class Pesanan18 {
+    int kodePesanan;
+    String namaPesanan;
+    int harga;
+
+    Pesanan18(int kodePesanan, String namaPesanan, int harga) {
+        this.kodePesanan = kodePesanan;
+        this.namaPesanan = namaPesanan;
+        this.harga = harga;
+    }
+}
