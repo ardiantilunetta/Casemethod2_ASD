8716a69c2adebeb61b0caeb475038641ba97ee6d
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2026-05-15 23:22:36 +0700

    class pembeli

---
+++ NodeAntrian18.java
@@ -0,0 +1,3 @@
+public class NodeAntrian18 {
+    
+}
---
+++ Pembeli18.java
@@ -0,0 +1,12 @@
+public class Pembeli18 {
+
+    String namaPembeli;
+    String noHp;
+
+    // Konstruktor pembeli, untuk menyimpan data pembeli
+    Pembeli18(String namaPembeli, String noHp) {
+        this.namaPembeli = namaPembeli;
+        this.noHp = noHp;
+    }
+    
+}   
