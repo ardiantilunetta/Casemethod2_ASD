a0951b814df3c989f806b733bad689f4b86a5280
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2026-05-15 23:25:49 +0700

    class node antrian

--- NodeAntrian18.java
+++ NodeAntrian18.java
@@ -1,3 +1,15 @@
 public class NodeAntrian18 {
     
+    int nomorAntrian;
+    Pembeli18 pembeli;
+
+    NodeAntrian18 prev;
+    NodeAntrian18 next;
+
+    NodeAntrian18(NodeAntrian18 prev, int nomorAntrian, Pembeli18 pembeli, NodeAntrian18 next) {
+        this.prev = prev;
+        this.nomorAntrian = nomorAntrian;
+        this.pembeli = pembeli;
+        this.next = next;
+    }
 }
